b021e1ae4f42240bacc8f60b8992455a2432f9a1
DaniloBMike <danilo7re@gmail.com>
2026-01-14 21:47:06 -0300

    Created comparative expressions

---
+++ src/comparativeexpressions.java
@@ -0,0 +1,14 @@
+public class comparativeexpressions {
+    public static void main (String [] args){
+
+        int ps5 = 50;
+        int xbox = 50;
+        int xbox2 = 75;
+        int ps52 = 90;
+
+        System.out.println(ps5 > xbox2);
+
+
+
+    }
+}
