79c1af14f8705505fc31a9352d2a0b243c4f31bc
DaniloBMike <danilo7re@gmail.com>
2026-01-21 23:18:14 -0300

    update

--- src/ifelsestructure.java
+++ src/ifelsestructure.java
@@ -1,7 +1,21 @@
 public class ifelsestructure {
     public static void main (String[] args) {
 
-        
+        int john = 75;
+        int maria = 80;
+        int evelyn = 67;
+        int danilo = 100;
 
+        boolean gradepass = true;
+
+        if (john <= 50) {
+            System.out.println("Denial");
+        }
+        else if (john <= 70){
+            System.out.println("Final test");
+        }
+        else {
+            System.out.println("Pass");
+        }
     }
 }
