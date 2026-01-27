7d2459c2ea91aa9bb8c1e824edfccda0767b0c81
DaniloBMike <danilo7re@gmail.com>
2026-01-26 22:39:53 -0300

    Created switch-case

---
+++ src/swicthcase.java
@@ -0,0 +1,28 @@
+public class swicthcase {
+    public static void main (String[] args) {
+
+        int age = 27;
+
+        switch (age) {
+            case 100:
+                System.out.println("There you are, the new Danilo");
+                break;
+
+            case 50:
+                System.out.println("You are old");
+                break;
+
+            case 80:
+                System.out.println("you are to older");
+                break;
+
+            default:
+                System.out.println("You are dead bro lol");
+        }
+
+        if (age >= 27);
+        System.out.println("There you are");
+
+
+    }
+}
