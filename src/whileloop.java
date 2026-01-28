78887cb75df107df50f3016568f65b7a7eceafb1
DaniloBMike <danilo7re@gmail.com>
2026-01-27 23:40:22 -0300

    Created while-loop

---
+++ src/whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop {
+    public static void main (String[] args) {
+
+        int count = 0;
+
+        while (count <=10) {
+            System.out.println("Counted : " + count);
+            count++;
+        }
+    }
+}
