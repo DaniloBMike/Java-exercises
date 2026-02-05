16060cf7509d292149b22948b7d78fee4f274417
DaniloBMike <danilo7re@gmail.com>
2026-02-04 22:55:47 -0300

    update

--- src/whileloop.java
+++ src/whileloop.java
@@ -1,15 +1,23 @@
 public class whileloop {
     public static void main (String[] args) {
 
-        int number = 100;
 
-        while (number >=10) {
-            System.out.println("The number is: " + number);
-            number++;
+
+               int number = 100;
+//
+//        while (number >=10) {
+//            System.out.println("The number is: " + number);
+//            number++;
+//        }
+
+
+
+
+        do {
+            System.out.println("count = " + number);
+            number--;
         }
-
-
-
+        while (number >5);
 
     }
     }
