7eea044daf5249fb777231d2c5d3af84364732f7
DaniloBMike <danilo7re@gmail.com>
2026-02-07 22:53:28 -0300

    update

---
+++ src/Array.java
@@ -0,0 +1,22 @@
+public class Array {
+    public static void main (String[] args) {
+
+        //Array declaration and initialization
+
+        int[] number = {10,20,85,5,64};
+        String[] names = {"Danilo, Vanessa, Silvana"};
+
+        double[] salary = new double[5];
+        salary[4] = 300.00;
+        salary[3] = 100.00;
+        salary[1] = 900.00;
+        salary[2] = 700.00;
+        salary[0] = 400.00;
+
+        for (int position = 0; position <salary.length; position++) {
+            
+        }
+
+
+    }
+}
--- src/whileloop.java
+++ src/whileloop.java
@@ -3,25 +3,34 @@ public class whileloop {
 
 
 
-//               int number = 100;
-//
-//        while (number >=10) {
-//            System.out.println("The number is: " + number);
-//            number++;
-//        }
+               int number = 100;
+
+        while (number <=10) {
+            System.out.println("The number is: " + number);
+            number--;
+        }
 
 
-//        do {
-//            System.out.println("Number = " + number);
-//            number++;
-//            }
-//        while (number <=15);
-//
-            for (int number = 100;number >=10; number--){
+        do {
+            System.out.println("Number = " + number);
+            number++;
+            }
+        while (number <=15);
+
+            for (;number >=10; number--){
                 System.out.println("number : " + number);
             }
 
+        int x = 10;
 
+        while (x > 0) {
+            if (x == 5) {
+                x--;
+                continue;
+            }
+            System.out.println(x);
+            x--;
+        }
 
     }
     }
