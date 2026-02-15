a8a0415b0c896493607ede87d5d1a2671f8b5989
DaniloBMike <danilo7re@gmail.com>
2026-02-14 23:12:17 -0300

    update Array

--- src/Array.java
+++ src/Array.java
@@ -9,20 +9,24 @@ public class Array {
 
         names = new String[] {"Danilo","Vanessa","Silvana"};
 
-//        double[] salary = new double[5];
-//        salary[3] = 900.00;
-//        salary[2] = 200.00;
-//        salary[1] = 300.00;
-//        salary[0] = 700.00;
-//        salary[4] = 400.00;
-//
-//        //Interaction with for
-//
+        double[] salary = new double[5];
+        salary[3] = 900.00;
+        salary[2] = 200.00;
+        salary[1] = 300.00;
+        salary[0] = 700.00;
+        salary[4] = 400.00;
+
+        //Interaction with for
+
 //        for (int position = 3; position < salary.length; position++) {
 //            System.out.println(salary[position]);
 //        }
 
-        
+        for (double income : salary) {
+            System.out.println(income);
+        }
 
+        salary[4] = 650.00;
+        System.out.println(salary[4]);
     }
 }
