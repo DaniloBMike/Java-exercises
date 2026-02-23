1193eee1f78f705008f44d44b065cc22a207b76a
DaniloBMike <danilo7re@gmail.com>
2026-02-22 21:36:35 -0300

    update

--- src/collectionwitharray.java
+++ src/collectionwitharray.java
@@ -11,34 +11,34 @@ public class collectionwitharray {
         familyNames.add("Vanessa");
         familyNames.add("Silvana");
 
-//        familyNames.add(2,"Enzo");
-//
-//        for (String names : familyNames) {
-//            System.out.println(names);
-//        }
-//
-//
-//        String names = familyNames.get(3);
-//        System.out.println("The position name is : " + names);
-//
-//
-//        familyNames.remove(3);
-//
-//        boolean evazia = familyNames.isEmpty();
-//
-//        int tamanho = familyNames.size();
-//
-//        boolean have = familyNames.contains("Danilo");
-//
-//        System.out.println("The list is empty ? " + evazia + " What is the size of the list ? " + tamanho
-//                + " Is Danilo a name of the list? " + have);
-//
-//        for (String names2 : familyNames) {
-//            System.out.println(names2);
-//        }
-//
-//        familyNames.clear();
-//        System.out.println("The list is empty now? " + familyNames.isEmpty());
+        familyNames.add(2,"Enzo");
+
+        for (String names : familyNames) {
+            System.out.println(names);
+        }
+
+
+        String names = familyNames.get(3);
+        System.out.println("The position name is : " + names);
+
+
+        familyNames.remove(3);
+
+        boolean evazia = familyNames.isEmpty();
+
+        int tamanho = familyNames.size();
+
+        boolean have = familyNames.contains("Danilo");
+
+        System.out.println("The list is empty ? " + evazia + " What is the size of the list ? " + tamanho
+                + " Is Danilo a name of the list? " + have);
+
+        for (String names2 : familyNames) {
+            System.out.println(names2);
+        }
+
+        familyNames.clear();
+        System.out.println("The list is empty now? " + familyNames.isEmpty());
 
 
                 familyNames.forEach(identity -> System.out.println(identity));
