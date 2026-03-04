2546a972baa64ce86494043e2f2133c7afe0f65c
DaniloBMike <danilo7re@gmail.com>
2026-03-04 19:20:02 -0300

    Collection HashMap
    Created new java class, called Collection HashMap

---
+++ src/collectionhashmap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class collectionhashmap {
+    public static void main (String[] args) {
+
+        HashMap<String, Double> money = new HashMap<>();
+        money.put("Danilo" , 1.500);
+        money.put("Vanessa" , 1.000);
+        money.put("Silvana" , 3.000);
+
+        money.get("Silvana");
+        System.out.println("Silvana has: " +);
+
+    }
+}
