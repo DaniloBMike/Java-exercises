4f1a2b56de859ef337a0f7af0c530e10f70a6895
DaniloBMike <danilo7re@gmail.com>
2026-03-17 09:17:17 -0300

    Review while structure

--- src/whileloop.java
+++ src/whileloop.java
@@ -1,36 +1,36 @@
 public class whileloop {
     public static void main (String[] args) {
 
+            int number = 10;
 
-
-               int number = 100;
-
-        while (number <=10) {
-            System.out.println("The number is: " + number);
-            number--;
-        }
-
-
-        do {
-            System.out.println("Number = " + number);
-            number++;
-            }
-        while (number <=15);
-
-            for (;number >=10; number--){
-                System.out.println("number : " + number);
+            while (number<=100) {
+                System.out.println("number = " + number);
+                number++;
             }
 
-        int x = 10;
 
-        while (x > 0) {
-            if (x == 5) {
-                x--;
-                continue;
-            }
-            System.out.println(x);
-            x--;
-        }
+
+
+//        do {
+//            System.out.println("Number = " + number);
+//            number++;
+//            }
+//        while (number <=15);
+//
+//            for (;number >=10; number--){
+//                System.out.println("number : " + number);
+//            }
+//
+//        int x = 10;
+//
+//        while (x > 0) {
+//            if (x == 5) {
+//                x--;
+//                continue;
+//            }
+//            System.out.println(x);
+//            x--;
+//        }
 
     }
     }
