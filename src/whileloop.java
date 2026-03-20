e33b972c5442c6846fb05d37bbb85cfc89b2cb56
DaniloBMike <danilo7re@gmail.com>
2026-03-20 10:32:52 -0300

    update

--- src/whileloop.java
+++ src/whileloop.java
@@ -3,37 +3,37 @@ public class whileloop {
 
             //while structure
 
-//            int number = 10;
+            int number = 10;
 
-//            while (number<=100) {
-//                System.out.println("The number is " + number);
-//                number++;
-//            }
+            while (number<=100) {
+                System.out.println("The number is " + number);
+                number++;
+            }
 
             //Do while structure
 
-//        do {
-//            System.out.println("Number = " + number);
-//            number++;
-//            }
-//        while (number <=15);
-//
+        do {
+            System.out.println("Number = " + number);
+            number++;
+            }
+        while (number <=15);
+
            // For structure
 
-           for(int number = 0; number < 10; number+=3) {
-               System.out.println("Numbers : " + number);
+           for(int numero = 0; numero < 10; numero+=3) {
+               System.out.println("Numbers : " + numero);
            }
-//
-//        int x = 10;
-//
-//        while (x > 0) {
-//            if (x == 5) {
-//                x--;
-//                continue;
-//            }
-//            System.out.println(x);
-//            x--;
-//        }
+
+        int x = 10;
+
+        while (x > 0) {
+            if (x == 5) {
+                x--;
+                continue;
+            }
+            System.out.println(x);
+            x--;
+        }
 
     }
     }
