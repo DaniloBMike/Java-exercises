938b3dbb3384500139580e10ea86cacd4c16f5ac
DaniloBMike <danilo7re@gmail.com>
2026-04-09 18:58:22 -0300

    update

--- src/collectionwitharray.java
+++ src/collectionwitharray.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class collectionwitharray {
+    public static void main (String[] args) {
+
+        List<String> names = new ArrayList<>();
+    }
+}
