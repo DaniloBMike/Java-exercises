ef98783ef722a1985e6bb6560142c3782c8cf6d0
DaniloBMike <danilo7re@gmail.com>
2026-04-10 11:18:27 -0300

    update
    update the collection and array list exercise

--- src/collectionwitharray.java
+++ src/collectionwitharray.java
@@ -4,6 +4,38 @@ import java.util.List;
 public class collectionwitharray {
     public static void main (String[] args) {
 
-        List<String> names = new ArrayList<>();
+        List<String> names = new ArrayList<>(); {
+
+            names.add("Danilo");
+            names.add("Silvana");
+            names.add("Vanessa");
+            names.add("Enzo");
+            names.add("Alice");
+            names.add("Cecilia");
+
+            names.add(1, "Manel coió");
+
+            names.remove("Danilo");
+            for (String name : names) {
+                System.out.println(name);
+            }
+
+            String name = names.get(5); {
+                System.out.println("The name is : " + name);
+            }
+
+            boolean isEmpty = names.isEmpty(); {
+                System.out.println("Is empty : " + isEmpty );
+            }
+
+            int size = names.size(); {
+                System.out.println("The size is : " + size);
+            }
+
+
+
+
+        }
+
     }
 }
