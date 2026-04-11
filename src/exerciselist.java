ad8f06ff24a57b91a4867d959b507409f18e55ff
DaniloBMike <danilo7re@gmail.com>
2026-04-11 12:24:45 -0300

    update the Array list exercise
    Finish the Array list exercise

--- src/exerciselist.java
+++ src/exerciselist.java
@@ -4,20 +4,33 @@ import java.util.List;
 public class exerciselist {
     public static void main (String[] args) {
 
-        List<String> names = new ArrayList<>();
+        List<String> students = new ArrayList<>();
 
-            names.add("Danilo");
-            names.add("Silvana");
-            names.add("Vanessa");
+        students.add("Ana");
+        students.add("Bruno");
+        students.add("Carlos");
+        students.add("Diana");
 
-        names.remove("Danilo"); {
-            System.out.println("The names are : " + names + " The size is : " + names.size());
+        students.add(2, "Eduardo");
+
+        students.remove(1);
+
+        int studant = students.size();
+        System.out.println(" The size is : " + studant);
+
+        String nameStudent = students.get(2);
+        System.out.println(" The third student is : " + nameStudent);
+
+        for (String allStudents : students) {
+            System.out.println(allStudents);
         }
 
-//        for (String name : names) {
-//            System.out.println("The names are : " + name);
-//        }
+        boolean empty = students.isEmpty();
+        System.out.println("The list is empty ? " + empty);
 
+        boolean contein = students.contains("Diana");
+        System.out.println("There are Diana in the list ? " + contein);
 
+        students.forEach(school -> System.out.println(school));
     }
 }
