417adb836553d260729dc6bcb9de10b5247ef1e5
DaniloBMike <danilo7re@gmail.com>
2026-04-25 10:44:03 -0300

    just an update

--- src/collectionhashmap.java
+++ src/collectionhashmap.java
@@ -3,23 +3,43 @@ import java.util.HashMap;
 public class collectionhashmap {
     public static void main (String[] args) {
 
-        HashMap<String, Integer> family = new HashMap<>();
 
-        family.put("Danilo", 27);
-        family.put("Silvana" , 40);
-        family.put("Vanessa" , 30);
+        HashMap<String, Integer> store = new HashMap<>();
 
-        //One of two method that we can get the key and the value
+        store.put("Apple",3);
+        store.put("Bread",7);
+        store.put("Milk", 5);
 
-        for (String familyName : family.keySet()) {
-            Integer ages = family.get(familyName);
-            System.out.println(familyName + " : " + ages);
+        Integer pao = store.get("Bread");
+        System.out.println("The value of Bread is : " + pao);
+
+        for (String stores : store.keySet()) {
+            Integer value = store.get(stores);
+            System.out.println(stores + " : " + value);
         }
 
-        //Getting just one ket and their value at time
 
-        Integer name = family.get("Silvana");
-        System.out.println("The name is : " + name);
+
+
+
+
+//        HashMap<String, Integer> family = new HashMap<>();
+//
+//        family.put("Danilo", 27);
+//        family.put("Silvana" , 40);
+//        family.put("Vanessa" , 30);
+//
+//        //One of two method that we can get the key and the value
+//
+//        for (String familyName : family.keySet()) {
+//            Integer ages = family.get(familyName);
+//            System.out.println(familyName + " : " + ages);
+//        }
+//
+//        //Getting just one ket and their value at time
+//
+//        Integer name = family.get("Silvana");
+//        System.out.println("The name is : " + name);
 
 
 
