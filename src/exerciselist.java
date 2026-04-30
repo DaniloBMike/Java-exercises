c5783387cd4bb178bac428ebb7b93f4a907c16f1
DaniloBMike <danilo7re@gmail.com>
2026-04-30 12:26:28 -0300

    finished Array List

--- src/exerciselist.java
+++ src/exerciselist.java
@@ -4,37 +4,57 @@ import java.util.List;
 public class exerciselist {
     public static void main (String[] args) {
 
-        List<String> students = new ArrayList<>();
+//        List<String> students = new ArrayList<>();
+//
+//        students.add("Ana");
+//        students.add("Bruno");
+//        students.add("Carlos");
+//        students.add("Diana");
+//
+//        students.add(2, "Eduardo");
+//
+//        students.remove(1);
+//
+//        int studant = students.size();
+//        System.out.println(" The size is : " + studant);
+//
+//        String nameStudent = students.get(2);
+//        System.out.println(" The third student is : " + nameStudent);
+//
+//        for (String allStudents : students) {
+//            System.out.println(allStudents);
+//        }
+//
+//        boolean empty = students.isEmpty();
+//        System.out.println("The list is empty ? " + empty);
+//
+//        boolean contein = students.contains("Diana");
+//        System.out.println("There are Diana in the list ? " + contein);
+//
+//        students.forEach(school -> System.out.println(school));
 
-        students.add("Ana");
-        students.add("Bruno");
-        students.add("Carlos");
-        students.add("Diana");
 
-        students.add(2, "Eduardo");
+        List<String> team = new ArrayList<>();
 
-        students.remove(1);
+        team.add("Ana");
+        team.add("Bob");
+        team.add("Carlos");
+        team.add("Diana");
+        team.add("Eduardo");
 
-        int studant = students.size();
-        System.out.println(" The size is : " + studant);
+        team.remove(2);
 
-        String nameStudent = students.get(2);
-        System.out.println(" The third student is : " + nameStudent);
+        team.add(1, "Felipe");
 
-        for (String allStudents : students) {
-            System.out.println(allStudents);
+
+        int sizeTeam = team.size();
+        System.out.println(" The size is  : " + sizeTeam);
+
+        for (String allnames : team){
+            System.out.println(allnames);
         }
 
-        boolean empty = students.isEmpty();
-        System.out.println("The list is empty ? " + empty);
 
-        boolean contein = students.contains("Diana");
-        System.out.println("There are Diana in the list ? " + contein);
-
-        students.forEach(school -> System.out.println(school));
     }
-
-
-
 
 }
