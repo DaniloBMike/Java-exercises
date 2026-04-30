316bf563a6fc83e175305ba3b93b598099f95439
DaniloBMike <danilo7re@gmail.com>
2026-04-30 12:25:10 -0300

    TreeMap
    created and finished exercises of treemap

---
+++ src/exercisetreemap.java
@@ -0,0 +1,22 @@
+import java.util.TreeMap;
+
+public class exercisetreemap {
+    public static void main (String[] args) {
+
+        TreeMap<String, Integer> scores = new TreeMap<>();
+
+        scores.put("Diana", 150);
+        scores.put("Alice", 200);
+        scores.put("Carlos", 180);
+        scores.put("Bruno", 220);
+
+        Integer dianasScore = scores.get("Diana");
+        System.out.println(" The Diana score is : " + dianasScore);
+
+        for (String allNames : scores.keySet()) {
+            Integer allscores = scores.get(allNames);
+            System.out.println(allNames + " : " + allscores);
+        }
+
+    }
+}
